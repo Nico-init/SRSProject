e72bf24d694018f974f2008b16017f9ce802da0e
luciano <luciano.massaccesi@mathfem.it>
2022-05-06 17:33:14 +0200

    defined DB interface functions

---
+++ src/utils/database.py
@@ -0,0 +1,29 @@
+
+
+
+def save_comment(user_id, comment_value: bool, reliability, stock_name, stock_value):
+    # must save date now
+    pass
+
+
+def get_users(days_num=None):
+    pass
+
+
+def get_user_comments(user_id, days_num):
+    return list_of[comment_value, reliability, stock_name, stock_value]
+
+
+# when save_comment, must call this
+def initialize_user(user_id):
+    # initialize points to zero and starting point to zero
+    pass
+
+
+def set_user_score(user_id, score):
+    pass
+
+
+def get_user_score(user_id):
+    return score_id
+
