ce463dab485bae9f48018698584ab68ef278ca54
Gianluca Soavi <gianluca.soavi@studio.unibo.it>
2022-05-31 09:34:18 +0000

    Updated scraper.py

--- src/scraper/scraper.py
+++ src/scraper/scraper.py
@@ -1,8 +1,9 @@
+import sys
+sys.path.insert( 0, './src' )
+
 import requests
 from bs4 import BeautifulSoup
 import re
-import sys
-sys.path.insert( 0, './src' )
 from utils.database import *
 from utils.comunication import *
 from utils.SRS_types import Comment
@@ -36,47 +37,61 @@ def scrape_value_yahoo(stock_name):
 
 def scrape_reddit(event):
     if event:
+        print("EVENT RECEIVED!")
+        print(" - Channel:\t'{}' \n - Metadata:\t{} \n - Body:\t{} \n - Tags:\t{}".format(event.channel, event.metadata, event.body, event.tags))
+        '''
         print("Subscriber Received Event: Metadata:'%s', Channel:'%s', Body:'%s tags:%s \n" % (
             event.metadata,
             event.channel,
             event.body,
             event.tags
         ))
-        
+        '''
         #es. event_body = user_id:comment_value:reliability:stock_name:date
         reddit_info = str(event.body.decode("UTF-8")).split(':')
+        #Convert positive/negative to bool
+        reddit_info[1] = reddit_info[1]=="positive"
         #print(reddit_info[0])
         #print(isinstance(reddit_info[0], str))
         #scrape stock value
         stock_value = scrape_value_yahoo(check_stock_name(reddit_info[3]))
-        #save comment on database
-        reddit_info[1] = reddit_info[1]=="positive"
-        save_comment(Comment(0, reddit_info[0], reddit_info[1], reddit_info[2], reddit_info[3], stock_value, reddit_info[4]))
+        #Check NoneType
+        if (stock_value is not None):
+            print("Comment:\n -> {} | {} | {} | {} | {} | {}".format(reddit_info[0], reddit_info[1], reddit_info[2], reddit_info[3], stock_value, reddit_info[4]))
+            print("Saving comment...")
+            #save comment on database
+            save_comment(Comment(0, reddit_info[0], reddit_info[1], reddit_info[2], reddit_info[3], stock_value, reddit_info[4]))
+            print("Comment saved")
+        else:
+            print("Stock {} not exist. Comment not saved.".format(reddit_info[3]))
         #debug
-        print(stock_value)
-       
+        #print(stock_value)
+
 
 def handle_incoming_error(error_msg):
-    print("received error:%s'" % (
+    print("received error: '%s'" % (
         error_msg
     ))
 
 def main():
     #read from the queue and scrape the value
-    print("Subscribing to event on channel testing_event_channel")
+    print("Hey there! THE SCRAPER IS BACK! =)")
+    print("----------------------------------")
+    #variables
+    channel = "testing_event_channel"
     cancel_token=ListenerCancellationToken()
 
-    #Retrieve the kubemw service ip
+    #Retrieve the kubemq service ip
     ip_server = socket.gethostbyname("kubemq")
     string_connection = ip_server+":"+"50000"
     try:
-        # Subscribe to events without store
+        #Subscribe to events without store
         #subscriber = Subscriber("10.0.86.232:50000")
         subscriber = Subscriber(string_connection)
-        
+
         print("Creating subscribe request..")
         subscribe_request = SubscribeRequest(
-            channel="testing_event_channel",
+            channel=channel,
             #client_id="hello-world-subscriber",
             client_id=socket.gethostname(),
             events_store_type=EventsStoreType.StartFromFirst,
@@ -85,7 +100,7 @@ def main():
             group="group1",
             subscribe_type=SubscribeType.EventsStore
         )
-        print("Subscribe request created")
+        print("Subscribe request for {} created".format(channel))
         print("Subscribing in progress..")
         subscriber.subscribe_to_events(subscribe_request, scrape_reddit, handle_incoming_error,cancel_token)
         print("Scraper subscribed successfully!")
